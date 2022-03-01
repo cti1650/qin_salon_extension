a5918abc427ecdb89193d17b9f23913546c02c91
cti1650 <cti1650@gmail.com>
2022-03-02 01:33:32 +0900

    次の動画を開く機能の実装

--- extensions/background.js
+++ extensions/background.js
@@ -28,3 +28,30 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 chrome.action.onClicked.addListener((tab) => {
   doScript(tab.url, tab.id);
 });
+
+chrome.runtime.onInstalled.addListener(async () => {
+  chrome.storage.local.get(["test_key"], ({ test_key }) => {
+    if (!test_key) {
+      test_key = [];
+      chrome.storage.local.set({ test_key });
+    }
+  });
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // console.log(request.message);
+  if (request.message) {
+    chrome.storage.local.set({ test_key: request.message }, function () {});
+    sendResponse(new Promise((resolve, reject) => {
+      // Asynchronously fetch all data from storage.local.
+      chrome.storage.local.get(['test_key'], ({ test_key }) => {
+        // Pass any observed errors down the promise chain.
+        if (chrome.runtime.lastError) {
+          return reject(chrome.runtime.lastError);
+        }
+        // Pass the data retrieved from storage down the promise chain.
+        resolve(test_key);
+      });
+    }));
+  }
+});
--- extensions/script.js
+++ extensions/script.js
@@ -1,3 +1,62 @@
 (() => {
-  
+  setTimeout(() => {
+    var dom = document.querySelectorAll(".MuiCard-root");
+    if (dom.length == 1) {
+      chrome.storage.local.get(["test_key"], ({ test_key }) => {
+        // Pass any observed errors down the promise chain.
+        if (chrome.runtime.lastError) {
+          return;
+        }
+        // console.log(test_key);
+        if (test_key) {
+          var content = document.querySelectorAll(".MuiCardContent-root");
+          if (content) {
+            const nextPage = test_key.filter((item,index) => (test_key[index - 1]?.url == location.href))[0];
+            const div = document.createElement("div");
+            div.classList.add("Extension-content");
+            if(!nextPage?.url){
+              div.innerHTML = `<div class="Extension-content__item">次の動画はありません。</div>`;
+            }else{
+              div.innerHTML = `<div class="Extension-content__item"><a href="${nextPage.url}">${nextPage.title} >> </a></div>`;
+            }
+            content[1].appendChild(div);
+          }
+        }
+      });
+    }
+  }, 1000);
+  setTimeout(() => {
+    var dom = document.querySelectorAll(".MuiCard-root");
+    if (dom.length > 1) {
+      let arr = [];
+      dom.forEach((item) => {
+        arr = [
+          ...arr,
+          {
+            category:
+              item.parentElement.parentElement.parentElement.getElementsByTagName(
+                "h2"
+              )[0].textContent,
+            title: item.innerText,
+            url: item.getElementsByTagName("a")[0].href,
+          },
+        ];
+      });
+      // console.log(arr);
+      chrome.runtime.sendMessage(
+        {
+          message: arr,
+        },
+        function () {
+          chrome.storage.local.get(["test_key"], ({ test_key }) => {
+            // Pass any observed errors down the promise chain.
+            if (chrome.runtime.lastError) {
+              return;
+            }
+            // console.log(test_key);
+          });
+        }
+      );
+    }
+  }, 2000);
 })();
